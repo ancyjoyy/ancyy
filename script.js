5e83c531cb24b39433307c4bf8deab79a80ea983
ancyjoyy <ancyjoy272727@gmail.com>
2025-02-14 01:45:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function checkError(){
+var checkError=document.getElementById('checkError');
+checkError.textContent="galat jawab! try again sweetie (^-^)";
+
+
+setTimeout(function() {
+    checkError.textContent = "";
+},1500);
+}
+
+
+
+
+
+
+
+
